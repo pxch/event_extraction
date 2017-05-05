f9b9917ddfdee2bed8c6002889499858b8d827f6
Pengxiang Cheng <pxcheng@utexas.edu>
2017-05-05 10:54:21 -0500

    add pipeline to sum up partial vocab counts

---
+++ sum_all_vocabs.py
@@ -0,0 +1,36 @@
+import argparse
+from bz2 import BZ2File
+from collections import defaultdict, Counter
+from os import listdir
+from os.path import isdir, join
+
+from util import read_counter, write_counter
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_path', help='directory to read vocabulary counts')
+parser.add_argument('output_path', help='directory to write vocabulary counts')
+
+args = parser.parse_args()
+
+input_dirs = sorted([join(args.input_path, f) for f in listdir(args.input_path)
+                     if isdir(join(args.input_path, f))])
+
+all_vocab = defaultdict(Counter)
+
+for input_dir in input_dirs:
+    print 'Reading vocabulary count from {}'.format(input_dir)
+    with BZ2File(join(input_dir, 'argument'), 'r') as fin:
+        all_vocab['argument'] += read_counter(fin)
+    with BZ2File(join(input_dir, 'name_entity'), 'r') as fin:
+        all_vocab['name_entity'] += read_counter(fin)
+    with BZ2File(join(input_dir, 'name_entity_tag'), 'r') as fin:
+        all_vocab['name_entity_tag'] += read_counter(fin)
+    with BZ2File(join(input_dir, 'predicate'), 'r') as fin:
+        all_vocab['predicate'] += read_counter(fin)
+    with BZ2File(join(input_dir, 'preposition'), 'r') as fin:
+        all_vocab['preposition'] += read_counter(fin)
+
+for key in all_vocab:
+    fout = BZ2File(join(args.output_path, key + '.bz2'), 'w')
+    write_counter(all_vocab[key], fout)
+    fout.close()
