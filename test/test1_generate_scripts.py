d55831fc3ac3727510840d4336053e3e6d855b2e
Pengxiang Cheng <pxcheng@utexas.edu>
2017-06-12 17:05:21 -0500

    add pipeline to generate stanford parsed evaluation dataset

--- test/test1_generate_scripts.py
+++ test/test1_generate_scripts.py
@@ -1,5 +1,7 @@
+import os
 import pickle as pkl
 
+from corenlp import read_doc_from_corenlp
 from ontonotes import read_all_docs_from_ontonotes
 from rich_script import Script
 
@@ -13,3 +15,21 @@ for doc in all_docs:
     all_scripts.append(script)
 
 pkl.dump(all_scripts, open('all_scripts.pkl', 'w'))
+
+corenlp_path = '/Users/pengxiang/corpora/ontonotes-release-5.0/output/parsed'
+
+corenlp_files = sorted([os.path.join(corenlp_path, f) for f
+                        in os.listdir(corenlp_path) if f.endswith('.xml')])
+
+all_docs_corenlp = [
+    read_doc_from_corenlp(open(corenlp_file))
+    for corenlp_file in corenlp_files]
+
+pkl.dump(all_docs_corenlp, open('all_docs_corenlp.pkl', 'w'))
+
+all_scripts_corenlp = []
+for doc in all_docs_corenlp:
+    script = Script.from_doc(doc)
+    all_scripts_corenlp.append(script)
+
+pkl.dump(all_scripts_corenlp, open('all_scripts_corenlp.pkl', 'w'))
